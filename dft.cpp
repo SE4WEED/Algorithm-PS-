7e756b2a9f824edc8af6757d3ac816bc2b319574
SE4WEED <121861351+SE4WEED@users.noreply.github.com>
2024-05-18 02:05:33 +0900

    dftcpp

---
+++ dft.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <string>
+using namespace std;
+void input(){
+
+
+    return ;
+}
+void solve(){
+
+
+    return ;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int cc;
+    cc = 1;
+    //cin >> cc;
+    for(int i=0;i<cc;i++){
+        input();
+        solve();
+    }
+}
